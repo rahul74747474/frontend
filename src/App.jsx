bc2d44eed17db40dfeab853485532d9b43942c26
rahul74747474 <ghibli141525@gmail.com>
2026-01-28 22:53:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
+import Navbar from "./Components/Navbar";
 import Incidents from "./Components/Incidents";
 import ChatBot from "./Components/ChatBot";
 
 function App() {
   return (
-    <div className="flex h-screen overflow-hidden bg-gray-100">
+    <div className="h-screen bg-slate-50 font-sans text-slate-900 selection:bg-indigo-100 selection:text-indigo-700">
+      <Navbar />
 
-      {/* Left: Incidents (scrolls independently) */}
-      <div className="flex-1 overflow-y-auto">
-        <Incidents />
+      <div className="flex h-[calc(100vh-64px)] overflow-hidden bg-gray-100">
+        {/* Left: Incidents */}
+        <div className="flex-1 overflow-y-auto">
+          <Incidents />
+        </div>
+
+        {/* Right: ChatBot */}
+        <div className="w-[30vw] overflow-y-auto">
+          <ChatBot />
+        </div>
       </div>
-      
-
     </div>
   );
 }
 
 export default App;
-
 
