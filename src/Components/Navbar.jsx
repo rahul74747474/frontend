70f4289bb160c69fb95d89b00f8c604f0f52cf96
rahul74747474 <ghibli141525@gmail.com>
2026-01-28 23:01:20 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,1 @@
-export default function Navbar() {
-  return (
-    <div className="sticky top-4 z-50 flex justify-center">
-      <div className="w-[95%] bg-white/80 backdrop-blur-md shadow-lg rounded-2xl px-6 py-3 flex justify-center items-center">
-        <img
-          src="https://i.ibb.co/8DdNcypz/1logo-removebg-preview.webp"
-          alt="Logo"
-          className="h-10 w-10 object-contain scale-300"
-        />
-      </div>
-    </div>
-  );
-}
+export default function Navbar() { return ( <div className="sticky top-4 z-50 flex justify-center"> <div className="w-[95%] bg-white/80 backdrop-blur-md shadow-lg rounded-2xl px-6 py-3 flex items-center gap-4"> <img src="https://i.ibb.co/8DdNcypz/1logo-removebg-preview.webp" alt="Logo" className="h-10 w-10 object-contain scale-200 ml-4" /> </div> </div> ); }
